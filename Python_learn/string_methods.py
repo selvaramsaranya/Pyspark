88b3184393e8b6c5c313b5fbb8d49272d01d06bc
Saranya <selvaram.saranya@gmail.com>
2024-05-29 14:25:54 -0400

    Initial commit

---
+++ Python_learn/string_methods.py
@@ -0,0 +1,13 @@
+# str = "Saranya is a good girl "
+# print(str.lstrip(str))
+# print(len(str))
+# print(str[0:str.find("g")])
+# print(str.islower())
+# print(str.lower())
+# print(str.strip())
+
+a = 10.08
+b= 10.08
+
+if(a==b):
+    print("pass")
